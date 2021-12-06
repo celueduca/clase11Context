6df600da5212e35331cbf34f5d246492b661f2bb
Juan Ortiz <jjortizsanta97@gmail.com>
2021-12-06 16:51:32 -0500

    Proteccion de rutas

--- Ejercicio/Juan-Jose-ortiz/src/App/App.tsx
+++ Ejercicio/Juan-Jose-ortiz/src/App/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { IUser } from "../interfaces/IUser";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -15,15 +15,24 @@ import ContactMe from "../pages/ContactMe";
 const App = () => {
   const [user, setUser] = useState<IUser>();
   const [usersApp, setUsersApp] = useState<IUser[]>(users);
+
+  const PrivateRoute = ({ component: Component, ...rest }: any) => (
+    <Route {...rest} render={(props: any) => (
+      user
+        ? <Component {...props} />
+        : <Redirect to="/" />
+    )} />
+  )
+
   return (
     <UsersContext.Provider value={{ users: usersApp, setUsers: setUsersApp }}>
       <AuthContext.Provider value={{ user, setUser: setUser }}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Login} />
-            <Route exact path="/home" component={Home} />
-            <Route exact path="/restore-password" component={RestorePassword} />
-            <Route exact path="/contact-me" component={ContactMe} />
+            <PrivateRoute exact path="/home" component={Home} />
+            <PrivateRoute exact path="/restore-password" component={RestorePassword} />
+            <PrivateRoute exact path="/contact-me" component={ContactMe} />
           </Switch>
         </BrowserRouter>
       </AuthContext.Provider>
